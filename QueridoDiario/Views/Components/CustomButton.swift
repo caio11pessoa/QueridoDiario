31a358995220ccd46623bb3166824e22f87818ed
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2025-04-07 18:22:47 -0300

    ✨ feat: Adiciona botões customizados
    Resolve: #5
    
    Adiciona botão customizado com 2 propriedadeso:
    
    Cor:
    - Branco
    - Lilás
    - Roxo
    
    Label:
    - Sem ícone
    - Ícone de mais
    - Ícone de seta pra direita

---
+++ QueridoDiario/Views/Components/CustomButton.swift
@@ -0,0 +1,115 @@
+//
+//  CustomButton.swift
+//  QueridoDiario
+//
+//  Created by Lais Godinho on 07/04/25.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    
+    var action: () -> Void = {}
+    
+    var buttonColor: ButtonColor = .lightPurple
+    
+    var buttonStyle: ButtonType = .plusIcon("Ação", .lightPurple)
+    
+    var body: some View {
+        Button(action: action, label: {
+            HStack {
+                Spacer()
+                buttonStyle.label
+                    .padding(.vertical, 8)
+                Spacer()
+            }
+//                .frame(width: 200, height: 50)
+        })
+        .background(buttonColor.background)
+        .cornerRadius(8)
+        .foregroundStyle(buttonColor.text)
+        .padding()
+    }
+}
+
+#Preview {
+    CustomButton()
+}
+
+enum ButtonColor {
+    case white
+    case lightPurple
+    case purple
+    
+    var background: Color {
+        switch self {
+        case .white:
+            return Color("neutrals_01")
+        case .lightPurple:
+            return Color("primary_02")
+        case .purple:
+            return Color("primary_04")
+        }
+    }
+    
+    var text: Color {
+        switch self {
+        case .white:
+            return Color("primary_06")
+        case .lightPurple:
+            return Color("primary_06")
+        case .purple:
+            return Color("neutrals_01")
+            
+        }
+        
+    }
+}
+    
+enum ButtonType {
+    case noIcon(String)
+    case plusIcon(String, ButtonColor)
+    case arrowIcon(String, ButtonColor)
+    
+    var label: AnyView {
+        switch self {
+        case .noIcon(let text):
+            return AnyView(Text("\(text)")
+                .font(Font.custom("BricolageGrotesque-ExtraBold", size: 24)))
+        case .plusIcon(let text, let color):
+            return AnyView(HStack {
+                Text("\(text)")
+                    .font(Font.custom("BricolageGrotesque-ExtraBold", size: 24))
+                Image("Icon=Plus, State=Select, Bg Fill=\(color == .purple ? "True" : "False")")
+            })
+        case .arrowIcon(let text, let color):
+            return AnyView(HStack {
+                Text("\(text)")
+                    .font(Font.custom("BricolageGrotesque-ExtraBold", size: 24))
+                Image("Icon=Right Arrow, State=Select, Bg Fill=\(color == .purple ? "True" : "False")")
+            })
+        }
+    }
+}
+
+
+//enum ButtonStyle {
+//    case white(String)
+//    case lightPurple(String)
+//    case purple(String)
+//    case whiteWithIcon(String, Image)
+//    case lightPurpleWithIcon(String, Image)
+//    case purpleWithIcon(String, Image)
+//    
+//    var label: any View {
+//        switch self {
+//            case .white(let text):
+//            case .lightPurple(let text):
+//            case .purple(let text):
+//                return Text("\(text)")
+//                .font(Font.custom("BricolageGrotesque-ExtraBold", size: 24))
+//            
+//                    
+//        }
+//    }
+//}
